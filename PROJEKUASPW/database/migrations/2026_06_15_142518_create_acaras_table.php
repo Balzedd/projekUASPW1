72c734a9939ed89b135a62d586a1f0d6c0febc39
Balzedd <baldzzzzeddd@gmail.com>
2026-06-16 12:07:20 +0700

    adro

--- PROJEKUASPW/database/migrations/2026_06_15_142518_create_acaras_table.php
+++ PROJEKUASPW/database/migrations/2026_06_15_142518_create_acaras_table.php
@@ -15,6 +15,9 @@ return new class extends Migration
             $table->id();
             $table->string('nama_acara');
             $table->text('deskripsi')->nullable();
+            $table->date('tanggal');
+            $table->string('lokasi');
+            $table->string('gambar')->nullable();
             $table->timestamps();
         });
     }
